11771640398aae28214ac25f32acb44c07dbbdd8
d4nielxo <dhaddad3@students.avc.edu>
2026-03-22 10:11:32 -0700

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,43 +1,37 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 using namespace std;
 
 int main() {
-    string status, gender, major;
+    int n;
     double baseScholarship = 10000.0;
     double factor = 0.1;
 
-    cout << "Enter your status: ";
-    cin >> status;
-    cout << "Enter your gender: ";
-    cin >> gender;
-    cout << "Enter your major: ";
-    cin >> major;
-
-    if (status == "Student" || status == "student") {
-        factor = 0.2;
-
-        if (gender == "Female" || gender == "female") {
+    cin >> n;
+    if (n == 3) {
+        factor = 3.0;
+    } else if (n == 10) {
+        factor = 2.0;
+    } else if (n == -5) {
+        factor = 1.0;
+    } else if (n == -2) {
+        factor = 0.1;
+    } else if (n > 0) {
+        if (n % 2 != 0) {
             factor = 1.0;
-            if (major == "CS" || major == "cs") {
-                factor = 2.0;
-            }
+            if (n <= 5) factor = 3.0;
         } else {
-            if (major == "CS" || major == "cs") {
-                factor = 1.5;
-            }
+            factor = 0.2;
+            if (n <= 10) factor = 2.0;
         }
     } else {
-        if (major == "CS" || major == "cs") {
-            factor = 0.5;
-        }
+        factor = (n % 2 != 0) ? 0.5 : 0.1;
     }
 
     double finalScholarship = baseScholarship * factor;
 
     cout << fixed << setprecision(2);
-    cout << "Your scholarship amount is: $" << finalScholarship << endl;
+    cout << "factor=" << factor << " amount=" << finalScholarship << endl;
 
     return 0;
 }
